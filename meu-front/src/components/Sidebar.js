a0b16b2b9cd57e8799e799df217e32339ea58f15
henriquesilva22 <henrique989151620@gmail.com>
2025-06-01 15:14:45 -0300

    Criação do react
    Criada as pastas para bases css basico imbutido nelas, criada pasta components com a estrutura ├── components/
    │   │   ├── Header.js
    │   │   ├── Header.css
    │   │   ├── Sidebar.js
    │   │   ├── Sidebar.css
    │   │   ├── Content.js
    │   │   └── Content.css
    │   ├── App.js
    │   └── App.css

---
+++ meu-front/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <p>Sub Menu</p>
+      <p>da Página</p>
+    </aside>
+  );
+}
+
+export default Sidebar;
